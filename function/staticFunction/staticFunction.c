516a2cc228ee27bfad4b33741e74bafdcb0c2197
MH-Rahid <mhrahid112@gmail.com>
2024-11-30 11:04:54 +0800

    staticFunction files added

---
+++ function/staticFunction/staticFunction.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+static void counterFunction() {
+    int count = 0;  // Static variable retains its value
+    count++;
+    printf("Counter: %d\n", count);
+}
+
+int main() {
+    counterFunction();  // Output: Counter: 1
+    counterFunction();  // Output: Counter: 2
+    counterFunction();  // Output: Counter: 3
+    return 0;
+}
