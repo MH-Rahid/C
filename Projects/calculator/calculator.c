0b87de78c81290e248bd5fbb845937070533de9b
MH-Rahid <mhrahid112@gmail.com>
2025-01-23 15:44:07 +0800

    added calculator project

--- Projects/calculator/calculator.c
+++ Projects/calculator/calculator.c
@@ -1,71 +1,99 @@
 #include <stdio.h>
-void addition(double num1, double num2);
-void subtraction(double num1, double num2);
-void multiplication(double num1, double num2);
-void division(double num1, double num2);
-void modulus(int num1, int num2);
+
+double addition(double num1, double num2);
+double subtraction(double num1, double num2);
+double multiplication(double num1, double num2);
+double division(double num1, double num2);
+int modulus(int num1, int num2);
 
 int main(int argc, char const* argv[])
 {
-    printf("\n********** Welcome to the simple Calculator! *********\n\n\n");
-    char count;
+
+    double num1, num2, result;
+    int choice;
+    printf("\n********** Welcome to the simple Calculator! *********\n");
 
 
     do
     {
-        char operator;
-        double x, y;
-        scanf("%lf %c %lf", &x, &operator, &y);
+        printf("\n\n1. Addition \n");
+        printf("2. Subtraction \n");
+        printf("3. Multiplication \n");
+        printf("4. Division \n");
+        printf("5. Modulus \n");
+        printf("6. Exit \n");
+        printf("Enter your Choice (1-6)\n");
+        scanf("%d", &choice);
 
+        if (choice >= 1 && choice <= 5) {
 
-        switch (operator)
-        {
-        case '+':
-            addition(x, y);
-            break;
-        case '-':
-            subtraction(x, y);
-            break;
-        case '*':
-            multiplication(x, y);
-            break;
-        case '/':
-            division(x, y);
-            break;
-
-        default:
-            printf("\n invalid inputs : %5.2lf %3c %5.2lf \n", x, operator, y);
+            printf("Enter two number: \n");
+            scanf("%lf%lf", &num1, &num2);
         }
 
+        switch (choice)
+        {
+        case 1:
+            result = addition(num1, num2);
+            printf("Result : %.2lf + %.2lf = %.2lf", num1, num2, result);
+            break;
+        case 2:
+            result = subtraction(num1, num2);
+            printf("Result : %.2lf - %.2lf = %.2lf", num1, num2, result);
 
-        printf("\n\n Do you want to perform another operation? (y/n) : ");
-        getchar();
-        scanf("%c", &count);
+            break;
+        case 3:
+            result = multiplication(num1, num2);
+            printf("Result : %.2lf * %.2lf = %.2lf", num1, num2, result);
 
-    } while (count == 'y' || count == 'Y');
+            break;
+        case 4:
+            result = division(num1, num2);
+            printf("Result : %.2lf / %.2lf = %.2lf", num1, num2, result);
+
+            break;
+        case 5:
+            result = modulus((int)num1, (int)num2);
+            printf("Result : %d %% %d = %d", (int)num1, (int)num2, (int)result);
+
+            break;
+        case 6:
+            printf("\nExit Successfully. \n");
+            break;
+        default:
+            printf("Invalid choice, Please select a valid options");
+        }
+    } while (choice != 6);
 
 
-    printf(" Thank you for using me.");
+
+    printf("\n\n************* Thank you for using me. **************");
 
     return 0;
 }
 
 
-void addition(double num1, double num2) {
-    printf("\n%20.2lf + %.2lf = %.2lf", num1, num2, (num1 + num2));
+double addition(double num1, double num2) {
+    return (num1 + num2);
 }
 
-void subtraction(double num1, double num2) {
-    printf("\n%20.2lf - %.2lf = %.2lf", num1, num2, (num1 - num2));
+double subtraction(double num1, double num2) {
+    return (num1 - num2);
 }
 
-void multiplication(double num1, double num2) {
-    printf("\n%20.2lf * %.2lf = %.2lf", num1, num2, (num1 * num2));
+double multiplication(double num1, double num2) {
+    return (num1 * num2);
 }
 
-void division(double num1, double num2) {
+double division(double num1, double num2) {
     if (num2 != 0)
-        printf("\n%20.2lf / %.2lf = %.2lf", num1, num2, (num1 / num2));
-    else
-        printf(" Division can not by zero");
+        return (num1 / num2);
+    else {
+        printf("Error:  Division by zero not allowed. \n");
+        return 0;
+    }
+}
+
+int modulus(int num1, int num2) {
+    return (num1 % num2);
 }
---
+++ Projects/numberGuessingGame/readme.txt
@@ -0,0 +1,10 @@
+Description: 
+This is fun project where the program generates a random number, and 
+the user has to guess it. The program provides feedback if the guess is 
+too high or too low.
+
+Key concepts Used: 
+1. rand() and srand() for generates random numbers.
+2. Loops (do-while) for repeated guessing.
+3. Conditional statements (if-else).
+
